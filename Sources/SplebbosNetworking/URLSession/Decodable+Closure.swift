e0d952669ad57e83847c75a480543f76f0b49640
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2022-07-23 13:07:55 +0100

    And again

--- Sources/SplebbosNetworking/URLSession/Decodable+Closure.swift
+++ Sources/SplebbosNetworking/URLSession/Decodable+Closure.swift
@@ -7,11 +7,13 @@ public extension URLSession {
     completion: @escaping (Result<T, Swift.Error>) -> Void
   ) {
     dataTask(for: resource) { result in
-      do {
-        let decoded = try decoder.decode(T.self, from: try result.get())
-        completion(.success(decoded))
-      } catch {
-        completion(.failure(error))
+      DispatchQueue.main.async {
+        do {
+          let decoded = try decoder.decode(T.self, from: try result.get())
+          completion(.success(decoded))
+        } catch {
+          completion(.failure(error))
+        }
       }
     }
   }
