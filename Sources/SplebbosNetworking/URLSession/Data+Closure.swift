5428a67113f69198666904d73056a8ca413af1d7
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2022-07-23 13:05:53 +0100

    Put closure on main thread

--- Sources/SplebbosNetworking/URLSession/Data+Closure.swift
+++ Sources/SplebbosNetworking/URLSession/Data+Closure.swift
@@ -2,18 +2,20 @@ import Foundation
 
 public extension URLSession {
   func dataTask(for resource: Resource, completionHandler: @escaping (Result<Data, Swift.Error>) -> Void) {
-    do {
-      let task = dataTask(with: try resource.request()) { data, response, error in
-        do {
-          let data = try self.getDataIfNoErrors(data, response, error)
-          completionHandler(.success(data))
-        } catch {
-          completionHandler(.failure(error))
+    DispatchQueue.main.async {
+      do {
+        let task = self.dataTask(with: try resource.request()) { data, response, error in
+          do {
+            let data = try self.getDataIfNoErrors(data, response, error)
+            completionHandler(.success(data))
+          } catch {
+            completionHandler(.failure(error))
+          }
         }
+        task.resume()
+      } catch {
+        completionHandler(.failure(error))
       }
-      task.resume()
-    } catch {
-      completionHandler(.failure(error))
     }
   }
 }
